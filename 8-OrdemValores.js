923c7d2419798b3a9e43bdc2f87de8da5b7aeee3
FIXER3600 <guilhermecomercial51@gmail.com>
2024-08-18 15:40:59 -0300

    exercicio 8

---
+++ 8-OrdemValores.js
@@ -0,0 +1,8 @@
+let valor1 = prompt("Digite o primeiro valor: ");
+let valor2 = prompt("Digite o segundo valor: ");
+
+if (valor1 < valor2) {
+    console.log("Os valores em ordem crescente são: " + valor1 + ", " + valor2);
+} else {
+    console.log("Os valores em ordem crescente são: " + valor2 + ", " + valor1);
+}
