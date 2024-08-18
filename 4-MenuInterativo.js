851c477e787623cfe99faf8c05d8e40e8f626022
FIXER3600 <guilhermecomercial51@gmail.com>
2024-08-18 15:35:30 -0300

    exercicio 4

---
+++ 4-MenuInterativo.js
@@ -0,0 +1,18 @@
+let opcao = prompt("Digite 1 para Opção 1, 2 para Opção 2, ou 3 para Opção 3: ");
+
+switch(opcao) {
+    case '1':
+        console.log("Você selecionou a Opção 1.");
+        // Aqui você pode adicionar a lógica para a Opção 1
+        break;
+    case '2':
+        console.log("Você selecionou a Opção 2.");
+        // Aqui você pode adicionar a lógica para a Opção 2
+        break;
+    case '3':
+        console.log("Você selecionou a Opção 3.");
+        // Aqui você pode adicionar a lógica para a Opção 3
+        break;
+    default:
+        console.log("Opção inválida. Por favor, digite 1, 2 ou 3.");
+}
